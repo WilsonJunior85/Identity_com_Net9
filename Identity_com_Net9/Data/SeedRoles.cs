2183d89cbf16362a3a9559c4e4961dd0927ae18a
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-09-03 11:51:41 -0300

    Criando Roles

---
+++ Identity_com_Net9/Data/SeedRoles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity_com_Net9.Data
+{
+    public class SeedRoles
+    {
+
+
+
+
+        public static async Task CreateRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roleNames = { "Admin", "User" };
+
+            foreach (var role in roleNames)
+            {
+
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}

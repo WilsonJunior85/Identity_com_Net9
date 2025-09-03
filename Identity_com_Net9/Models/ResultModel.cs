6f14b953b83e9c475e7f1c31f65f3d4968e3ec31
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-09-03 11:36:47 -0300

    Models

---
+++ Identity_com_Net9/Models/ResultModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity_com_Net9.Models
+{
+    public class ResultModel<T>
+    {
+        public T? Data { get; set; }
+
+        public string Mensagem { get; set; } = string.Empty;
+
+        public bool Status { get; set; } = true;
+    }
+}

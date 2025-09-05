f3c695f0e957443ce1ca866366b283402bb27af6
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-09-05 11:07:23 -0300

    Instalando e configurando o Scalar

---
+++ Identity_com_Net9/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Identity_com_Net9.Dto;
+using Identity_com_Net9.Services.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_com_Net9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var resultado = await _authService.Register(registerDto);
+            if (!resultado.Status) return BadRequest(resultado);
+            return Ok(resultado);
+
+
+
+
+        }
+    }
+}
--- Identity_com_Net9/Services/Auth/AuthService.cs
+++ Identity_com_Net9/Services/Auth/AuthService.cs
@@ -1,6 +1,78 @@
-﻿namespace Identity_com_Net9.Services.Auth
+﻿using Identity_com_Net9.Dto;
+using Identity_com_Net9.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity_com_Net9.Services.Auth
 {
-    public class AuthService: IAuthService
+    public class AuthService : IAuthService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public async Task<ResultModel<string>> Register(RegisterDto registerDto)
+        {
+
+            ResultModel<string> result = new ResultModel<string>();
+
+            try
+            {
+                var user = new ApplicationUser
+                {
+                    Email = registerDto.Email,
+                    NomeCompleto = registerDto.NomeCompleto,
+                    UserName = registerDto.Usuario
+                };
+
+                var resultado = await _userManager.CreateAsync(user, registerDto.Senha);
+
+                if (!resultado.Succeeded) 
+                {
+                    result.Data = string.Join(",", resultado.Errors.Select( e => e.Description));
+                    result.Status = false;
+                    return result;
+                }
+
+                var rolesInvalidas = new List<string>();
+                foreach (var role in registerDto.Roles) 
+                { 
+                  if(!await _roleManager.RoleExistsAsync(role))
+                    {
+                        rolesInvalidas.Add(role);
+                    }
+                }
+
+                if (rolesInvalidas.Any())
+                {
+                    result.Data = $"As seguintes roles não existem: {string.Join(", ", rolesInvalidas)}";
+                    result.Status = false;
+                    return result;
+                }
+
+                foreach (var role in registerDto.Roles) 
+                { 
+                  
+                   await _userManager.AddToRoleAsync(user, role);
+                
+                }
+
+
+                result.Mensagem = "Usuário cadastrado com sucesso";
+                return result;
+
+            }
+            catch (Exception ex) 
+            { 
+                result.Mensagem = ex.Message;
+                result.Status = false;
+                return result;
+            }
+        }
     }
 }
